0c740db191973b5f383cc386d19bec8361052d10
carmine <carmine.992@gmail.com>
2014-05-17 22:13:11 +0200

    fix null pointer exception

--- BouncyRun/src/it/carmine/bouncyrun/MainActivity.java
+++ BouncyRun/src/it/carmine/bouncyrun/MainActivity.java
@@ -55,13 +55,13 @@ public class MainActivity extends Activity {
 		gs=new GameSettings();
 		
 		if(checkNick()){
-			havePreviousNick();
+			noHavePreviousNick(nick);
 		}else{
-			noHavePreviousNick();
+			noHavePreviousNick(null);
 		}
 	}
 	
-	private void noHavePreviousNick(){
+	private void noHavePreviousNick(String nick){
 		DisplayMetrics metrics = new DisplayMetrics();
 		getWindowManager().getDefaultDisplay().getMetrics(metrics);
 		int height=metrics.heightPixels;
@@ -180,6 +180,11 @@ public class MainActivity extends Activity {
 				
 		b.setOnClickListener(new OnClick_());
 		
+		if(nick!=null){
+			EditText et=(EditText)dialog.findViewById(R.id.editText1);
+			et.setText(nick);
+		}
+		
 		dialog.setCancelable(false);
 		dialog.show();
 	}
@@ -188,119 +193,8 @@ public class MainActivity extends Activity {
 	private void saveNickname(String n){
 		sql.insertNick(n);
 	}
-	private void havePreviousNick(){
-		DisplayMetrics metrics = new DisplayMetrics();
-		getWindowManager().getDefaultDisplay().getMetrics(metrics);
-		int height=metrics.heightPixels;
-		int width=metrics.widthPixels;
-		
-		
-		FrameLayout fl=new FrameLayout(this);
-		fl.setBackgroundColor(getResources().getColor(R.color.bgcolor));
-		
-		gw=new GameView(this,width,height);
-		gw.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, 
-				LayoutParams.MATCH_PARENT));
-		
-		gw.setOnGameOverListener(new GameOverListner(){
-			@Override
-			public void onGameOver(int points) {
-				final Dialog dialog1 = new Dialog(MainActivity.this,R.style.PauseDialog);
-				dialog1.requestWindowFeature(Window.FEATURE_NO_TITLE);
-				dialog1.setContentView(R.layout.custom_alert_gameover);
-				dialog1.setCancelable(false);
-				TextView tv=(TextView)dialog1.findViewById(R.id.textView1);
-				//tv.setText(tv.getText().toString()+points);
-				Typeface tf = Typeface.createFromAsset(MainActivity.this.getAssets(),
-						"font/pipe.ttf");
-				tv.setTypeface(tf);
-				tv.setTextColor(Color.BLACK);
-				
-				
-				ImageView facebook_share=(ImageView) dialog1.findViewById(R.id.facebook_share);
-				ImageView altro_share=(ImageView)dialog1.findViewById(R.id.google_share);
-				ImageView altro_classifica=(ImageView)dialog1.findViewById(R.id.altro_share);
-				
-				Button close=(Button)dialog1.findViewById(R.id.button1);
-				Button regame=(Button)dialog1.findViewById(R.id.button2);
-				
-				dialog1.show();
-
-				close.setOnClickListener(new OnClickListener(){
-					@Override
-					public void onClick(View v) {
-						mustclose=true;
-						finish();					
-					}
-					
-				});
-				regame.setOnClickListener(new OnClickListener(){
-					@Override
-					public void onClick(View v) {
-						finish();
-						startActivity(new Intent(MainActivity.this,MainActivity.class));
-					}
-				});
-				facebook_share.setOnClickListener(new OnClickListener(){
-					@Override
-					public void onClick(View v) {
-						if(isOnline()){
-							Intent i=new Intent(MainActivity.this,FacebookLoginActivity.class);
-							i.putExtra("points", gw.getPoint().getP()+"");
-							i.putExtra("nick", gw.getNick());
-							
-							startActivity(i);
-							
-							dialog1.cancel();
-							MainActivity.this.finish();
-						}else{
-							Toast toast=Toast.makeText(MainActivity.this,
-									"Problema connessione internet",Toast.LENGTH_LONG);
-							toast.show();
-						}
-					}
-				});
-				
-				altro_share.setOnClickListener(new OnClickListener(){
-					@Override
-					public void onClick(View v) {
-						Intent sendIntent = new Intent();
-						sendIntent.setAction(Intent.ACTION_SEND);
-						sendIntent.putExtra(
-								Intent.EXTRA_TEXT, "Ho raggiungo un punteggio di "+
-										gw.getPoint().getP()+" su BouncyRun, scaricalo anche tu!"+
-											" url"
-										);
-						sendIntent.setType("text/plain");
-						startActivity(sendIntent);
-					}
-				});
-				
-				altro_classifica.setOnClickListener(new OnClickListener(){
-					@Override
-					public void onClick(View v) {
-						Intent i=new Intent(MainActivity.this,ClassificaActivity.class);
-						i.putExtra("nick",  gw.getNick());
-						i.putExtra("difficolta", gw.getDifficolta());
-						i.putExtra("punteggio", gw.getPoint().getP()+"");
-						startActivity(i);
-					}
-				});
-			}
-		});
-		
-		fl.addView(gw);
-		fl.addView(addCloseButton());
-		setContentView(fl);
-		
-		//questo è da cambiare, devo selezionarlo in una dialog!
-		gs.setDifficult(gs.DIFFICULT_EASY);
-		
-		gs.setUserSettings(new UserSettings());
-		gs.getUserSettings().setNick(nick);
-		
 	
-	}
+	
 	private boolean checkNick(){
 		Cursor c=sql.fetchNick();
 		if(c.getCount()>0){
