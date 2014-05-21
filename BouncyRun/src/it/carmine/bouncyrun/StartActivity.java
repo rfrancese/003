bb4443dd47ffa99c2926537290e8dfb7af3817ff
carmine <carmine.992@gmail.com>
2014-05-21 22:19:47 +0200

    sviluppo splash

--- BouncyRun/src/it/carmine/bouncyrun/StartActivity.java
+++ BouncyRun/src/it/carmine/bouncyrun/StartActivity.java
@@ -1,9 +1,13 @@
 package it.carmine.bouncyrun;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.AsyncTaskLoader;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.os.AsyncTask;
 
 public class StartActivity extends Activity {
 
@@ -11,5 +15,23 @@ public class StartActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_start);
+		
+		
+	}
+	
+	class Asinc extends AsyncTask{
+		@Override
+		public void onPostExecute(Object o){
+			startActivity(new Intent(StartActivity.this,MainActivity.class));
+		}
+		@Override
+		protected Object doInBackground(Object... o){
+			try {
+				Thread.sleep(2500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
 	}
 }
